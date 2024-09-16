249b18b34a4ced98dc89ff6f20bd0bb1e048c68e
Andrew Aliaj <andyaliaj@gmail.com>
2024-09-16 16:00:29 -0400

    new route task

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
 });
 
+// Corrected route definition
+app.get('/task', (req, res) => {
+  res.send("New tasks");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
